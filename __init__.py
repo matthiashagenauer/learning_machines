2f5ccca6502459454b6dfc8381f86ff825e64699
matthiashagenauer <74023324+matthiashagenauer@users.noreply.github.com>
2025-06-23 10:43:49 +0200

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,10 +1,11 @@
 #from .test_actions import run_all_actions
 #from .playground import run_all_playground_actions
 #from .assignment_1 import run_training, controller
-from .assignment_2.PPO import train_ppo
+#from .assignment_2.PPO import train_ppo
+from .assignment_2.assignment_2 import controller
 
 
-__all__ = ("train_ppo")
+__all__ = ("controller")
 #__all__ = ("run_all_playground_actions", "run_training", "controller")
 #__all__ = ("run_all_actions",)
 #__all__ = ("run_training",)
